37ebb256b216e59f3ba670cd7d29caab3a170457
Wei Qu <wei.qu@domain.com.au>
2020-01-18 16:03:16 +1100

    test

--- codes/train.py
+++ codes/train.py
@@ -1,15 +1,10 @@
 from __future__ import print_function, absolute_import, division
 import glob
-import tensorflow as tf
-# from keras.backend.tensorflow_backend import set_session
-# config = tf.ConfigProto()
-# config.gpu_options.per_process_gpu_memory_fraction = 0.9
-# set_session(tf.Session(config=config))
-import numpy as np
 from collections import Counter
+
 np.random.seed(1)
 from tensorflow import set_random_seed
-from keras.callbacks import LearningRateScheduler
+
 set_random_seed(1)
 from datetime import datetime
 import argparse
@@ -18,7 +13,6 @@ from keras.utils import to_categorical, plot_model
 from keras.layers import Flatten, Dense
 from keras.models import Model
 from keras.optimizers import Adamax as opt
-from keras import optimizers
 from keras.callbacks import ModelCheckpoint, TensorBoard, CSVLogger
 import pandas as pd
 
@@ -26,14 +20,11 @@ from modules import *
 from utils import *
 from AudioDataGenerator import *
 
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("fold",
                         help="data csvfile to use")
-    parser.add_argument("--num_class", type=int,
-                        help="One of {5,6}- Classification method")
     parser.add_argument("--seed", type=int,
                         help="Random seed")
     parser.add_argument("--loadmodel",
@@ -49,20 +40,16 @@ if __name__ == '__main__':
     parser.add_argument("--comment",
                         help="Add comments to the log files")
 
-
     args = parser.parse_args()
     print("%s selected" % (args.fold))
-    foldname = args.fold
+    fold_idx = args.fold
 
     if args.seed:  # if random seed is specified
         print("Random seed specified as %d" % (args.seed))
         random_seed = args.seed
     else:
         random_seed = 1
-    if args.num_class:
-        num_class = args.num_class
-    else:
-        num_class =5
+    num_class = 5
 
     if args.loadmodel:  # If a previously trained model is loaded for retraining
         load_path = args.loadmodel  #### path to model to be loaded
@@ -100,11 +87,10 @@ if __name__ == '__main__':
     else:
         comment = None
 
-
-    model_dir = os.path.join(os.getcwd(),'..','models').replace('\\', '/')
-    fold_dir = os.path.join(os.getcwd(),'..','data').replace('\\', '/')
-    log_dir = os.path.join(os.getcwd(),'..','logs').replace('\\', '/')
-    log_name = foldname + '_' + str(datetime.now()).replace(':','-')
+    model_dir = os.path.join(os.getcwd(), '..', 'models').replace('\\', '/')
+    fold_dir = os.path.join(os.getcwd(), '..', 'data').replace('\\', '/')
+    log_dir = os.path.join(os.getcwd(), '..', 'logs').replace('\\', '/')
+    log_name = fold_idx + '_' + str(datetime.now()).replace(':', '-')
 
     if not os.path.exists(os.path.join(model_dir, log_name).replace('\\', '/')):
         new_dir = (os.path.join(model_dir, log_name).replace('\\', '/'))
@@ -116,15 +102,15 @@ if __name__ == '__main__':
         print(new_dir)
         os.makedirs(new_dir)
 
-    checkpoint_name = os.path.join(model_dir,log_name,'weights.{epoch:04d}-{val_acc:.4f}.hdf5').replace('\\', '/')
-    results_file = os.path.join(os.getcwd(), '..', 'results.csv').replace('\\','/')
+    checkpoint_name = os.path.join(model_dir, log_name, 'weights.{epoch:04d}-{val_acc:.4f}.hdf5').replace('\\', '/')
+    results_file = os.path.join(os.getcwd(), '..', 'results.csv').replace('\\', '/')
 
     params = {
 
         'num_classes': num_class,
         'batch_size': batch_size,
         'epochs': epochs,
-        'aafoldname': foldname,
+        'aafoldname': fold_idx,
         'random_seed': random_seed,
         'load_path': load_path,
         'shuffle': True,
@@ -132,39 +118,38 @@ if __name__ == '__main__':
         'eeg_length': 3000,
         'kernel_size': 16,
         'bias': True,
-        'maxnorm': 400000000000., ## No maxnorm constraint
-        'dropout_rate': 0.45, #.5
+        'maxnorm': 400000000000.,  ## No maxnorm constraint
+        'dropout_rate': 0.45,  # .5
         'dropout_rate_dense': 0.,
         'padding': 'valid',
         'activation_function': 'relu',
         'subsam': 2,
         'trainable': True,
-        'lr': .001, #.0001
-        'lr_decay': 0.0 #1e-5, #1e-5
+        'lr': .001,  # .0001
+        'lr_decay': 0.0  # 1e-5, #1e-5
     }
 
-
-    current_learning_rate= params['lr']
+    current_learning_rate = params['lr']
     inf = glob.glob(os.path.join(fold_dir, 'SC*.csv'))
     df = pd.DataFrame()
     for f in inf:
         df = pd.concat([df, pd.read_csv(f)])
-    trainX, valX, trainY, valY, pat_train, pat_val = patientSplitter(df)
+    trainX, valX, trainY, valY, pat_train, pat_val = patientSplitter(df, fold_idx)
     print(trainX.shape, valX.shape, trainY.shape, valY.shape)
     del df
 
     print("Data loaded")
-    
+
     if args.classweights:
         params['class_weight'] = compute_weight(trainY.astype(int), np.unique(trainY.astype(int)))
     else:
         params['class_weight'] = dict(zip(np.r_[0:params['num_classes']], np.ones(params['num_classes'])))
 
-    print('Classwise data in train',Counter(trainY))
-	
+    print('Classwise data in train', Counter(trainY))
+
     trainY = to_categorical(trainY)
     valY = to_categorical(valY)
-    trainX = np.expand_dims(trainX,axis=-1)
+    trainX = np.expand_dims(trainX, axis=-1)
     valX = np.expand_dims(valX, axis=-1)
 
     K.clear_session()
@@ -178,7 +163,7 @@ if __name__ == '__main__':
     if load_path:
         model.load_weights(filepath=load_path, by_name=False)
     model_json = model.to_json()
-    with open(os.path.join(model_dir, log_name, 'model.json').replace('\\','/'), "w") as json_file:
+    with open(os.path.join(model_dir, log_name, 'model.json').replace('\\', '/'), "w") as json_file:
         json_file.write(model_json)
 
     model.compile(optimizer=opt(lr=params['lr'], epsilon=None, decay=params['lr_decay']),
@@ -191,40 +176,40 @@ if __name__ == '__main__':
 
     tensdir = log_dir + "/" + log_name + "/"
     tensdir = tensdir.replace('/', "\\")
-    tensbd = TensorBoard(log_dir=tensdir, batch_size=batch_size, write_grads=True,)
-    patlogDirectory = log_dir +'/'+ log_name +'/'
-    trainingCSVdirectory = log_dir +'/'+ log_name +'/'+ 'training.csv'
+    tensbd = TensorBoard(log_dir=tensdir, batch_size=batch_size, write_grads=True, )
+    patlogDirectory = log_dir + '/' + log_name + '/'
+    trainingCSVdirectory = log_dir + '/' + log_name + '/' + 'training.csv'
     csv_logger = CSVLogger(trainingCSVdirectory)
-
 
     ####### Training #########
 
     try:
 
         datagen = AudioDataGenerator(
-                                      # shift=.1,
-                                      roll_range=.15,
-                                      samplewise_center=True,
-                                      samplewise_std_normalization=True,
-                                             )
+            # shift=.1,
+            roll_range=.15,
+            samplewise_center=True,
+            samplewise_std_normalization=True,
+        )
 
         valgen = AudioDataGenerator(
 
-              samplewise_center=True,
-              samplewise_std_normalization=True,
+            samplewise_center=True,
+            samplewise_std_normalization=True,
         )
 
-        model.fit_generator(datagen.flow(trainX, trainY, batch_size=params['batch_size'], shuffle=True, seed=params['random_seed']),
-                            steps_per_epoch=len(trainX) // params['batch_size'],
-                            epochs=params['epochs'],
-                            validation_data=valgen.flow(valX, valY, batch_size=params['batch_size'],
-                                                        seed=params['random_seed']),
-                            callbacks=[modelcheckpnt,csv_logger, tensbd, lrate],
-                            class_weight=params['class_weight']
-                            )
+        model.fit_generator(
+            datagen.flow(trainX, trainY, batch_size=params['batch_size'], shuffle=True, seed=params['random_seed']),
+            steps_per_epoch=len(trainX) // params['batch_size'],
+            epochs=params['epochs'],
+            validation_data=valgen.flow(valX, valY, batch_size=params['batch_size'],
+                                        seed=params['random_seed']),
+            callbacks=[modelcheckpnt, csv_logger, tensbd, lrate],
+            class_weight=params['class_weight']
+            )
 
-        results_log(results_file=results_file, log_dir=log_dir, log_name= log_name, params=params)
+        results_log(results_file=results_file, log_dir=log_dir, log_name=log_name, params=params)
 
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
-        results_log(results_file=results_file, log_dir=log_dir, log_name= log_name, params=params)
+        results_log(results_file=results_file, log_dir=log_dir, log_name=log_name, params=params)
--- codes/utils.py
+++ codes/utils.py
@@ -1,22 +1,18 @@
 from __future__ import print_function, absolute_import, division
 import numpy as np
+
 np.random.seed(1)
 from tensorflow import set_random_seed
+
 set_random_seed(1)
-from datetime import datetime
-import argparse
 import os
-from keras.utils import to_categorical, plot_model
-from keras.layers import Flatten, Dense
-from keras.models import Model
-from keras.optimizers import Adamax as opt
+import re
 from keras.callbacks import ModelCheckpoint, TensorBoard, CSVLogger, Callback
 import pandas as pd
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-from sklearn.utils import class_weight
 from modules import *
 from keras.callbacks import LearningRateScheduler
+
 
 def step_decay(global_epoch_counter):
     lrate = .001
@@ -25,6 +21,7 @@ def step_decay(global_epoch_counter):
         if global_epoch_counter > 20:
             lrate = .001 / 100
     return lrate
+
 
 lrate = LearningRateScheduler(step_decay)
 
@@ -35,8 +32,9 @@ def standardnormalization(distribution):
     data = np.expand_dims(data, axis=1)
     scaler = StandardScaler()
     outdata = scaler.fit_transform(data)
-    toutdata = outdata.reshape(int(outdata.shape[0]/3000),3000)
+    toutdata = outdata.reshape(int(outdata.shape[0] / 3000), 3000)
     return toutdata
+
 
 def compute_weight(Y, classes):
     num_samples = len(Y)
@@ -47,30 +45,84 @@ def compute_weight(Y, classes):
     class_weights = {i: (num_samples / (n_classes * num_bin[i])) for i in range(5)}
     return class_weights
 
-def patientSplitter(data,task='SC',stages=5):
-	
-    trainFile = os.path.join('..','benchmark',task+'_train.csv').replace('\\', '/')
-    testFile = os.path.join('..','benchmark',task+'_test.csv').replace('\\', '/')
-    df = pd.read_csv(trainFile)
-    print(type(data))
-    print(type(data.iloc[: 3002]))
-    trainMask = np.asarray([each in df.iloc[:, 0].values for each in data.iloc[:, 3002]])
-    df = pd.read_csv(testFile)
-    testMask = np.asarray([each in df.iloc[:, 0].values for each in data.iloc[:, 3002]])
-	
-    X_train = data[trainMask].values[:,:3000]
-    X_test= data[testMask].values[:,:3000]
-    Y_train= data.iloc[:, 3000][trainMask].values
-    Y_test= data.iloc[:, 3000][testMask].values
-    pat_train=[int(each[2:5]) for each in data.iloc[:, 3002][trainMask].values]
-    pat_test= [int(each[2:5]) for each in data.iloc[:, 3002][testMask].values]
-    return X_train,X_test,Y_train,Y_test,pat_train,pat_test
+
+def train_test_split(fold_idx):
+    files = [
+        'SC4001E0',
+        'SC4002E0',
+        'SC4011E0',
+        'SC4012E0',
+        'SC4021E0',
+        'SC4022E0',
+        'SC4031E0',
+        'SC4032E0',
+        'SC4041E0',
+        'SC4042E0',
+        'SC4051E0',
+        'SC4052E0',
+        'SC4061E0',
+        'SC4062E0',
+        'SC4071E0',
+        'SC4072E0',
+        'SC4081E0',
+        'SC4082E0',
+        'SC4091E0',
+        'SC4092E0',
+        'SC4101E0',
+        'SC4102E0',
+        'SC4111E0',
+        'SC4112E0',
+        'SC4121E0',
+        'SC4122E0',
+        'SC4131E0',
+        'SC4141E0',
+        'SC4142E0',
+        'SC4151E0',
+        'SC4152E0',
+        'SC4161E0',
+        'SC4162E0',
+        'SC4171E0',
+        'SC4172E0',
+        'SC4181E0',
+        'SC4182E0',
+        'SC4191E0',
+        'SC4192E0'
+    ]
+    subject_files = []
+    for f in files:
+        if fold_idx < 10:
+            pattern = re.compile("[a-zA-Z0-9]*0{}[1-9]E0\.npz$".format(fold_idx))
+        else:
+            pattern = re.compile("[a-zA-Z0-9]*{}[1-9]E0\.npz$".format(fold_idx))
+        if pattern.match(f):
+            subject_files.append(f)
+    train_files = list(set(files) - set(subject_files))
+    train_files.sort()
+    subject_files.sort()
+    return train_files, subject_files
+
+
+def patientSplitter(data, fold_idx):
+    tr, te = train_test_split(fold_idx)
+    print('train: ', tr)
+    print('test: ', te)
+    trainMask = np.asarray([each in tr for each in data.iloc[:, 3002]])
+    testMask = np.asarray([each in tr for each in data.iloc[:, 3002]])
+
+    X_train = data[trainMask].values[:, :3000]
+    X_test = data[testMask].values[:, :3000]
+    Y_train = data.iloc[:, 3000][trainMask].values
+    Y_test = data.iloc[:, 3000][testMask].values
+    pat_train = [int(each[2:5]) for each in data.iloc[:, 3002][trainMask].values]
+    pat_test = [int(each[2:5]) for each in data.iloc[:, 3002][testMask].values]
+    return X_train, X_test, Y_train, Y_test, pat_train, pat_test
+
 
 def results_log(results_file, log_dir, log_name, params):
     df = pd.read_csv(results_file)
     df1 = pd.read_csv(os.path.join(log_dir, log_name, 'training.csv').replace('\\', '/'))
 
-    new_entry= params
+    new_entry = params
     new_entry.pop('class_weight')
     new_entry.pop('lr')
     a = df1.head()
@@ -84,19 +136,19 @@ class log_metrics(Callback):
     def __init__(self, valX, valY, patID, patlogDirectory, global_epoch_counter, **kwargs):
 
         self.patID = patID
-        super(log_metrics,self).__init__(**kwargs)
+        super(log_metrics, self).__init__(**kwargs)
         self.patlogDirectory = patlogDirectory
         self.global_epoch_counter = global_epoch_counter
         self.valY = np.argmax(valY, axis=-1)
         # self.valY = np.expand_dims(self.valY, axis=1)
         self.valX = valX - np.mean(valX, keepdims=True)
-        self.valX /= (np.std(self.valX,keepdims=True) + K.epsilon())
+        self.valX /= (np.std(self.valX, keepdims=True) + K.epsilon())
 
-    def accuracy_score(self,true,predY):
+    def accuracy_score(self, true, predY):
         match = sum(predY == true)
-        return match/len(true)
+        return match / len(true)
 
-    def calcMetrics(self,predY,mask=None):
+    def calcMetrics(self, predY, mask=None):
 
         if mask is None:
             mask = np.ones(shape=self.valY.shape).astype(bool)
@@ -113,10 +165,10 @@ class log_metrics(Callback):
             # each = int(each)
             sens.append(confMat[each, each] / sum(confMat[each, :]))
             spec.append((match - confMat[each, each]) / (
-            (match - confMat[each, each] + sum(confMat[:, each]) - confMat[each, each])))
-            acc.append(match/(match+ sum(confMat[:, each] + sum(confMat[each, :] -2*confMat[each, each]))))
+                (match - confMat[each, each] + sum(confMat[:, each]) - confMat[each, each])))
+            acc.append(match / (match + sum(confMat[:, each] + sum(confMat[each, :] - 2 * confMat[each, each]))))
 
-        return sens,spec,acc
+        return sens, spec, acc
 
     def on_epoch_end(self, epoch, logs):
 
@@ -125,9 +177,9 @@ class log_metrics(Callback):
             predY = self.model.predict(self.valX, verbose=0)
             predY = np.argmax(predY, axis=-1)
 
-            sens,spec,acc = self.calcMetrics(predY)
+            sens, spec, acc = self.calcMetrics(predY)
 
-            for sens_,spec_,acc_,class_ in zip(sens,spec,acc,set(self.valY)):
+            for sens_, spec_, acc_, class_ in zip(sens, spec, acc, set(self.valY)):
                 logs["Class%d-Sens" % class_] = sens_
                 logs["Class%d-Spec" % class_] = spec_
                 logs["Class%d-Acc" % class_] = acc_
@@ -135,22 +187,22 @@ class log_metrics(Callback):
             patAcc = []
             for pat in np.unique(self.patID).astype(int):
                 mask = self.patID[:, 0] == pat
-                acc = self.accuracy_score(self.valY[mask],predY[mask])
+                acc = self.accuracy_score(self.valY[mask], predY[mask])
                 patAcc.append(acc)
             logs["patAcc"] = np.mean(patAcc)
 
-            sens,spec,acc = self.calcMetrics(predY,self.patID[:,0] <= 39)
+            sens, spec, acc = self.calcMetrics(predY, self.patID[:, 0] <= 39)
             logs['SC-Sens'] = np.mean(sens)
             logs['SC-Spec'] = np.mean(spec)
             logs['SC-Acc'] = np.mean(acc)
 
-            if sum(self.patID[:,0] > 39):
-                sens,spec,acc = self.calcMetrics(predY,self.patID[:,0] > 39)
+            if sum(self.patID[:, 0] > 39):
+                sens, spec, acc = self.calcMetrics(predY, self.patID[:, 0] > 39)
                 logs['ST-Sens'] = np.mean(sens)
                 logs['ST-Spec'] = np.mean(spec)
                 logs['ST-Acc'] = np.mean(acc)
 
-            self.global_epoch_counter = self.global_epoch_counter +1
+            self.global_epoch_counter = self.global_epoch_counter + 1
 
             ##############
             lr = self.model.optimizer.lr
@@ -158,6 +210,6 @@ class log_metrics(Callback):
                 lr *= (1. / (1. + self.model.optimizer.decay * K.cast(self.model.optimizer.iterations,
                                                                       K.dtype(self.model.optimizer.decay))))
             t = K.cast(self.model.optimizer.iterations, K.floatx()) + 1
-            lr_t = lr * (K.sqrt(1. - K.pow(self.model.optimizer.beta_2, t)) / (1. - K.pow(self.model.optimizer.beta_1, t)))
+            lr_t = lr * (K.sqrt(1. - K.pow(self.model.optimizer.beta_2, t)) / (
+                        1. - K.pow(self.model.optimizer.beta_1, t)))
             logs['lr'] = np.array(float(K.get_value(lr_t)))
-
